e4d9c42577f1d0ea10869bffbb5960f87731c5f2
AleksandrTan <rumych@list.ru>
2017-03-28 17:28:12 +0300

    myfirst1

--- bfg/bfg/wsgi.py
+++ bfg/bfg/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('e:/WebProjects/bfg.com/bfg/')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bfg.settings")
+# os.environ["DJANGO_SETTINGS_MODULE"] = "mydgango.settings"
 
 application = get_wsgi_application()
