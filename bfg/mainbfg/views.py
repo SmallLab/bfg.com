d8458737a26e7ebc61fe3c12fa1b35a065b010cb
AleksandrTan <rumych@list.ru>
2017-03-28 17:46:47 +0300

    Add settings file

--- bfg/mainbfg/views.py
+++ bfg/mainbfg/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def index(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    #return HttpResponse(html)
+    return HttpResponse("Hello Alex!!! You future salary is - 3500$!!!It is very cool!!!Be happy!!!")
