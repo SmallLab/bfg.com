5296995a9b587fdad0b6acf3f797f0e63995f063
AleksandrTan <rumych@list.ru>
2017-04-06 22:58:54 +0300

    Templates

--- bfg/mainbfg/mainviews/views.py
+++ bfg/mainbfg/mainviews/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.contrib import messages
+from django.conf import settings
 
 def index(request):
    messages.info(request, 'Three credits remain in your account.')
    content = "Hello Alex!!! You future salary is - 4500$!!!It is very cool!!!Be happy!!!"
    storage = messages.get_messages(request)
    d = []
+   d.append(settings.BASE_DIR)
    for message in storage:
       d.append(message)
    return HttpResponse(d)
