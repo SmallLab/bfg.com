9240f35cf5f449962e354f0551210d456385f303
AleksandrTan <rumych@list.ru>
2017-04-10 11:58:03 +0300

    Add directories in app

--- bfg/mainbfg/mainviews/views.py
+++ bfg/mainbfg/mainviews/views.py
@@ -7,7 +7,6 @@ def index(request):
    content = "Hello Alex!!! You future salary is - 4500$!!!It is very cool!!!Be happy!!!"
    storage = messages.get_messages(request)
    d = []
-   d.append(settings.BASE_DIR)
    for message in storage:
       d.append(message)
    return HttpResponse(d)
