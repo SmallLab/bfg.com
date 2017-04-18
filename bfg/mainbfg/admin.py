3c8d87b4147b43c8bba6e1a08e9915bd3fb58b8e
AleksandrTan <rumych@list.ru>
2017-04-18 23:07:46 +0300

    Add some models

--- bfg/mainbfg/admin.py
+++ bfg/mainbfg/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TypeSentence, Categories
 
-# Register your models here.
+admin.site.register(TypeSentence)
+admin.site.register(Categories)
--- bfg/mainbfg/models.py
+++ bfg/mainbfg/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+"""
+Model for type sentence
+"""
+
+class TypeSentence(models.Model):
+    name = models.CharField(max_length=50)
+    link_name = models.CharField(max_length=50, default='')
+    is_active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return "/%s/" % self.link_name
+
+    def __str__(self):
+        return self.name
+
+
+"""
+    Model fo categories sentence
+"""
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+    link_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return "/%s/" % self.link_name
+
+    def __str__(self):
+        return self.name
