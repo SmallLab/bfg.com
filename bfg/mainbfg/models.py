ee80d3fc88ef486989a6e7650089ceeaa7b40688
AleksandrTan <rumych@list.ru>
2017-04-20 11:57:43 +0300

    Add some templates

--- bfg/mainbfg/models.py
+++ bfg/mainbfg/models.py
@@ -4,14 +4,13 @@ from django.db import models
 """
 Custom Manadger for model TypeSentence
 """
+
+
 class ManadgerTypeSentence(models.Manager):
 #Get active Types
     def get_active_types(self):
         return self.get_queryset().filter(is_active__exact= True)
 
-"""
-Model for type sentence
-"""
 class TypeSentence(models.Model):
     name = models.CharField(max_length=50)
     link_name = models.CharField(max_length=50, default='')
@@ -28,18 +27,21 @@ class TypeSentence(models.Model):
 """
 Custom Manadger for model Categories
 """
+
+
 class ManadgerCategories(models.Manager):
 #Get active Categories
     def get_active_categories(self):
-        return self.get_queryset().filter(is_active__exact = True)
+        activecategories = self.get_queryset().filter(is_active__exact = True).values_list('name', 'icon_style', 'is_icon_img')
+        return [activecategories[i:i+5] for i in range(0, len(activecategories), 5)]
 
-"""
-    Model fo categories sentence
-"""
+
 class Categories(models.Model):
     name = models.CharField(max_length=50)
     link_name = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
+    icon_style = models.CharField(max_length=50, default='')
+    is_icon_img = models.BooleanField(default=False)
     object = ManadgerCategories()
 
     def get_absolute_url(self):
@@ -49,6 +51,7 @@ class Categories(models.Model):
         return self.name
 
 ################################# Regions Model ##################################
+
 
 class Regions(models.Model):
     name = models.CharField(max_length=100)
