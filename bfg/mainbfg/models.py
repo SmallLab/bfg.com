cbc8429041c8a916c353c7d5733b3d0e7eba478f
AleksandrTan <rumych@list.ru>
2017-05-05 12:58:28 +0300

    Add some models

--- bfg/mainbfg/models.py
+++ bfg/mainbfg/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-#------------------------- TypeSentence Model -----------------------------------------------
+#------------------------- TypeSentence Model -----------------------------------------------#
 """
 Custom Manadger for model TypeSentence
 """
@@ -35,6 +38,7 @@ class ManadgerCategories(models.Manager):
     def get_active_categories(self):
         return self.get_queryset().filter(is_active__exact = True)
 
+#Get categories on 5 ps.
     def get_list_categories(self):
         activecategories = self.get_queryset().filter(is_active__exact = True)
         return [activecategories[i:i+5] for i in range(0, len(activecategories), 5)]
@@ -66,3 +70,54 @@ class Regions(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+#-------------------------------- Sentence Model -------------------------------------------#
+
+class Sentence(models.Model):
+
+    type_id = models.SmallIntegerField()
+    category_id = models.SmallIntegerField()
+    sub_id = models.SmallIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    autor = models.CharField(max_length=100)
+    caption = models.CharField(max_length=200)
+    region_id = models.SmallIntegerField()
+    full_adress = models.CharField(max_length=250, blank=True)
+    phone = models.CharField(max_length=100, blank=True)
+    web = models.CharField(max_length=250, blank=True)
+    is_webstore = models.BooleanField(default=False)
+    description = models.TextField()
+    main_img = models.CharField(max_length=250, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    stop_time = models.DateTimeField(blank=True)
+    status = models.SmallIntegerField(default=0)
+    type_s = models.SmallIntegerField(default=0)
+    type_img_s = models.CharField(max_length=300)
+    meta_info = models.CharField(max_length=1000, blank=True)
+    views = models.IntegerField(default=0)
+    phone_views = models.IntegerField(default=0)
+    text_message = models.CharField(max_length=1000, blank=True)
+    is_paid = models.BooleanField(default=False)
+    start_time_paid = models.DateTimeField(blank=True)
+    end_time_paid = models.DateTimeField(blank=True)
+    on_moderation = models.BooleanField(default=False)
+
+#-------------------------------- Profile Model -------------------------------------------#
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(blank=True, max_length=100)
+    email = models.EmailField(blank=True, max_length=255)
+    favorite_num = models.SmallIntegerField(blank=True, default=0)
+    count_sentence = models.SmallIntegerField(blank=True, default=0)
+    is_subscrabtion = models.BooleanField(default=False)
+    is_subsciber = models.BooleanField(default=False)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+
+#-------------------------------- Payments Model -------------------------------------------#
+
+class Payment(models.Model):
+    pass
