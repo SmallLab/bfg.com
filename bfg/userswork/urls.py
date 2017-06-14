94c53f0ab2ffb1f6ead14a39eec18783935613c1
AleksandrTan <rumych@list.ru>
2017-06-14 23:21:12 +0300

    User office

--- bfg/userswork/urls.py
+++ bfg/userswork/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
    url(r'^addsentense/$', views.ShowFormSentenseView.as_view(), name='showform'),
    url(r'^createsentence/$', views.CreateNewSentence.as_view(), name='createsentence'),
    url(r'^privateoffice/(?P<tab>[\w]*)/?$', views.PrivateOfficeView.as_view(), name='privateoffice'),
-   url(r'^privateoffice/delete/(?P<pk>\d+)/$', views.PODeleteSentenceView.as_view(), name='deletesent')
+   url(r'^privateoffice/delete/(?P<pk>\d+)/$', views.PODeleteSentenceView.as_view(), name='deletesent'),
+   url(r'^privateoffice/deactive/(?P<pk>\d+)/$', views.PODeactiveSentenceView.as_view(), name='deactivesent')
 ]
