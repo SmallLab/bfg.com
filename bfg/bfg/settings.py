b177536a077149e5bd39f7658d2e236090091063
AleksandrTan <rumych@list.ru>
2017-06-30 20:16:33 +0300

    User office

--- bfg/bfg/settings.py
+++ bfg/bfg/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'bfg.mainbfg.mainhelpers.trytoclose.CheckUsers',
+    'mainbfg.mainhelpers.trytoclose.CheckUsers',
 ]
 
 ROOT_URLCONF = 'bfg.urls'
--- bfg/mainbfg/mainhelpers/trytoclose.py
+++ bfg/mainbfg/mainhelpers/trytoclose.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse, HttpResponseNotFound
 class CheckUsers(object):
     def process_request(self, request):
         if not request.user.is_authenticated():
-            return HttpResponseNotFound('<h1>Page not found</h1>')
+            return HttpResponseNotFound('<h1>Page not founds</h1>')
