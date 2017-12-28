51f5a365bdcce4d62fd680c13734b3090d8035de
AleksandrTan <rumych@list.ru>
2017-12-28 20:34:23 +0200

    User office

--- bfg/mainbfg/mainviews/views.py
+++ bfg/mainbfg/mainviews/views.py
@@ -14,7 +14,6 @@ class MainView(TemplateView):
    def get_context_data(self, **kwargs):
        context = super(MainView, self).get_context_data(**kwargs)
        context['data'] = self.get_data_ctr()
-       #context['sentences'] = Sentence.objects.get(id=72)
        return context
 
 
