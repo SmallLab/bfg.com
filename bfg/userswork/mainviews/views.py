658015bb9ff28aadbb9319facf3f62f73d1ab4bc
AleksandrTan <rumych@list.ru>
2017-12-28 11:17:43 +0200

    User office

--- bfg/userswork/mainviews/views.py
+++ bfg/userswork/mainviews/views.py
@@ -87,6 +87,8 @@ class POEditSentenceView(LoginRequiredMixin, UpdateView):
     context_object_name = 'data'
     template_name = 'editsent.html'
     success_url = '/user/privateoffice/'
+    type_img_s = {1: 'images/label-01-102x75.png', 2: 'images/label-02-105x95.png',
+                  3: 'images/label-03-98-71.png'}
 
     def get_context_data(self, **kwargs):
         context = super(POEditSentenceView, self).get_context_data(**kwargs)
@@ -94,6 +96,73 @@ class POEditSentenceView(LoginRequiredMixin, UpdateView):
         context['range_img'] = range(7)
 
         return context
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.type_img_s = self.type_img_s[form.cleaned_data['type_id']]
+        instance.link_name = self.slugify(form.cleaned_data['caption']) + '#' + instance.identifier
+        # delete main file photo
+        if int(self.request.POST['is_del_mainphoto']) == 0:
+            instance.main_img.delete()
+            instance.main_img = 'nophoto.png'
+        if int(self.request.POST['is_del_other_photo']) == 1:
+            self.delete_related_photo(instance, self.request.POST['list_del_other_photo'])
+        instance.save()
+        self.save_oter_files(instance, form)
+
+        return super(POEditSentenceView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        context = self.get_context_data()
+        context['data'] = self.request.POST
+
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return self.success_url
+
+
+    def slugify(swlf, str):
+        import re
+        import unidecode
+        return re.sub(r'\s+', '-', unidecode.unidecode(str).lower().strip())
+
+    def uuid_sentece_user(self):
+        import uuid
+        return 'user_'+str(uuid.uuid4())[:10]
+
+    def uuid_sentece(self):
+        import uuid
+        return str(uuid.uuid4())[:10]
+
+    def save_oter_files(self, instance, form):
+        if not os.path.isdir(settings.TEST_MEDIA_IMAGES+instance.dirname_img) and self.request.FILES.getlist('other_img[]'):
+            os.mkdir(settings.TEST_MEDIA_IMAGES+instance.dirname_img, mode=0o777)
+        #https://docs.djangoproject.com/ja/1.11/_modules/django/utils/datastructures/ - look for MultiValueDict(getlist)
+        if self.request.FILES.getlist('other_img[]'):
+            for ifile in self.request.FILES.getlist('other_img[]'):
+                if ifile.size < settings.MAX_SIZE_UPLOAD and ifile.content_type in settings.CONTENT_TYPES_FILE:
+                    fs = FileSystemStorage(location=settings.TEST_MEDIA_IMAGES+instance.dirname_img,
+                                           base_url=settings.TEST_MEDIA_IMAGES+instance.dirname_img)
+                    filename = fs.save(ifile.name, ifile)
+                    i = Image(sentence=instance,
+                              img_path=fs.url(filename))
+                    i.save()
+                else:
+                    #messages.info(self.request, 'Three credits remain in your account.')
+                    continue
+        return True
+
+    def delete_related_photo(self, instance, file_list):
+        import json
+        data = json.loads(file_list)
+        for imaje in data:
+            img = Image.objects.filter(id=imaje).get()
+            try:
+                os.remove(settings.BASE_DIR +'/'+ img.img_path)
+                img.delete()
+            except OSError:
+               pass
 
 """
     Create new sentence
@@ -103,7 +172,7 @@ class CreateNewSentence(LoginRequiredMixin, CreateView):
     login_url = 'login'
     form_class = SentenceForm
     template_name = 'addsentens.html'
-    succes_url = '/user/privateoffice/'
+    success_url = '/user/privateoffice/'
     type_img_s = {1:'images/label-01-102x75.png', 2:'images/label-02-105x95.png',
                   3:'images/label-03-98-71.png'}
 
@@ -133,7 +202,7 @@ class CreateNewSentence(LoginRequiredMixin, CreateView):
         return context
 
     def get_success_url(self):
-        return self.succes_url
+        return self.success_url
 
 
     def slugify(swlf, str):
