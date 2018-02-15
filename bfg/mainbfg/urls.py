1d32ae07ed850d045225495ba5f502291af5124c
AleksandrTan <rumych@list.ru>
2018-02-15 23:02:39 +0200

    Filter page

--- bfg/mainbfg/urls.py
+++ bfg/mainbfg/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.MainView.as_view(), name='index'),
     url(r'^categories/(?P<link_name>[a-z0-9-]+)/(?:type/(?P<type>\w+)/)?(?:page/(?P<page>\d+)/)?$', views.CategoryPage.as_view(), name='categorypage'),
     url(r'^filtersent/(?:page/(?P<page>\d+)/)?$', views.FilterSentences.as_view(), name='filtersent'),
+    url(r'^alltop/(?:page/(?P<page>\d+)/)?$', views.AllTop.as_view(), name='alltop'),
     url(r'^sentence/.+_(?P<slug>.+)/$', views.ViewSentence.as_view(), name='detail'),
     url(r'^sentence/showphone/$', views.ShowPhone.as_view(), name='showphone'),
     url(r'^registrations/$', vv.RegistrationUser, name='reristrations'),
