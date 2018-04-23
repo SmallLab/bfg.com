80f9b5810d427b60a14c273f189e4a574c0dc275
AleksandrTan <rumych@list.ru>
2018-04-23 12:10:26 +0300

    Subscration with login/register

--- bfg/mainbfg/static/js/script.js
+++ bfg/mainbfg/static/js/script.js
@@ -42,16 +42,52 @@ function SubServise(e, obj) {
     this.is_auth_status = data_sub.is_auth;
     this.init = function () {
         this.e.preventDefault();
-        this.is_auth_status == 0 ? this.loginSub() : this.addSub();
+        this.is_auth_status == 0 ? this.loginSub() : this.addDataSub();
+        $('[name="email_sms"]').on('click', this.enterData);
+        $('#addSub').on('click', this.addSub());
     };
     this.loginSub = function () {
         $('#enterSystem').attr('href', $('#enterSystem').attr('href')+'&id_sent='+this.id_sub);
         this.popup_is_login.modal('show');
     };
 
-    this.addSub = function () {
+    this.addDataSub = function () {
         this.popup_is_auth.modal('show');
     };
+
+    this.addSub = function () {
+
+    };
+
+    this.enterData = function () {
+            if ($(this).attr('id') == 'sms_data'){
+                if(data_sub.phone){
+                    $('#dataSub').val(data_sub.phone);
+                }
+                else {
+                    $('#dataSub').val('');
+                    $('#dataSub').attr('placeholder', 'Введите телефон (999)123-45-67');
+                }
+            }
+            else{
+                if (data_sub.email) {
+                    $('#dataSub').val(data_sub.email);
+                }
+                else {
+                    $('#dataSub').val('');
+                    $('#dataSub').attr('placeholder', 'Введите EMAIL');
+                }
+            }
+        };
+    this.checkEmail = function(email) {
+        var regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        return regex.test(email);
+    };
+    
+    this.checkPhone = function (phone) {
+        var regex = /^\([\d]{2,3}\)\ [\d]{2,3}-[\d]{2,3}-[\d]{2,3}$/;
+        return regex.test(phone);
+    }
 }
 //Show modal window if user is auth and wath redirect
 function isSubscrabers() {
